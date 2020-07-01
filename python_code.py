7c9eaf6c1a1f943e558893cc5063c8edf365ac6c
PREMSAIBHYRI1106 <premsaibhyri234@gmail.com>
2020-07-01 16:04:46 +0530

    add

--- python_code.py
+++ python_code.py
@@ -1,1 +1,9 @@
+# this notebook is mainly for Capstone project of IBM DATA SCIENCE SPECIALIZATIOn 
+
+### github has many intresting and unique features
+
+
+
+import pandas as pd
+import numpy as np
 print("Hello world")
